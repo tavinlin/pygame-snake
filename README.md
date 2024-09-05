85c0c9df1f83cd3890372e0f25aad08139bd972a
Tavin <tavinlin1519@gmail.com>
2024-09-05 11:12:55 -0400

    Project initialized

---
+++ README.md
@@ -0,0 +1,9 @@
+# Pygame - Snake
+This snake game was created using the Pygame library. Currently you can move the snake and eat food but there is many features that needs to be added in order to make this game complete.
+
+### Here is list of features will be added:
+- Display score
+- Border to prevent the snake from going out of boundary
+- End the game when the snake collide the border or itself
+
+### Develpment Log:
---
+++ main.py
@@ -0,0 +1,61 @@
+import pygame
+import random
+
+pygame.init()
+square_width = 620
+pixel_width = 25
+screen = pygame.display.set_mode([square_width] * 2)
+clock = pygame.time.Clock()
+running = True
+
+
+def generate_starting_position():
+    position_range = (pixel_width // 2, square_width - pixel_width // 2, pixel_width)
+    return [random.randrange(*position_range), random.randrange(*position_range)]
+
+
+snake_pixel = pygame.rect.Rect([0, 0, pixel_width - 2, pixel_width - 2])
+snake_pixel.center = generate_starting_position()
+snake = [snake_pixel.copy()]
+snake_direction = (0, 0)
+snake_length = 3
+
+target = pygame.rect.Rect([0, 0, pixel_width - 2, pixel_width - 2])
+target.center = generate_starting_position()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("black")
+
+    if snake_pixel.center == target.center:
+        target.center = generate_starting_position()
+        snake_length += 1
+        snake.append(snake_pixel.copy())
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        snake_direction = (0, - pixel_width)
+    if keys[pygame.K_s]:
+        snake_direction = (0, pixel_width)
+    if keys[pygame.K_a]:
+        snake_direction = (- pixel_width, 0)
+    if keys[pygame.K_d]:
+        snake_direction = (pixel_width, 0)
+
+    for snake_part in snake:
+        pygame.draw.rect(screen, "green", snake_part)
+
+    pygame.draw.rect(screen, "red", target)
+
+    snake_pixel.move_ip(snake_direction)
+    snake.append(snake_pixel.copy())
+    snake = snake[-snake_length:]
+
+    pygame.display.flip()
+
+    clock.tick(10)
+
+pygame.quit()
